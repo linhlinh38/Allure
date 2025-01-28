6bc3862b7d0511595e91a0635f4141506f96e395
linhlinh38 <03linhnguyen@gmail.com>
2025-01-28 19:51:35 +0700

    consultant service implement

--- src/entities/livestream.entity.ts
+++ src/entities/livestream.entity.ts
@@ -1,38 +1,32 @@
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-} from 'typeorm';
-import { BaseEntity } from './base.entity';
-import { StatusEnum } from '../utils/enum';
-import { Account } from './account.entity';
-import { Order } from './order.entity';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from "typeorm";
+import { BaseEntity } from "./base.entity";
+import { StatusEnum } from "../utils/enum";
+import { Account } from "./account.entity";
+import { Order } from "./order.entity";
 
-@Entity('livestreams')
+@Entity("live_streams")
 export class LiveStream extends BaseEntity {
-  @Column({ type: 'varchar', length: 100, nullable: true })
+  @Column({ type: "varchar", length: 100, nullable: true })
   title: string;
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: "timestamp" })
   startTime: Date;
 
-  @Column({ type: 'timestamp' })
+  @Column({ type: "timestamp" })
   endTime: Date;
 
-  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Column({ type: "varchar", length: 255, nullable: true })
   record: string;
 
   @Column({
-    type: 'enum',
+    type: "enum",
     enum: StatusEnum,
     default: StatusEnum.ACTIVE,
   })
   status: StatusEnum;
 
   @ManyToOne(() => Account, (account) => account.files)
-  @JoinColumn({ name: 'accountId' })
+  @JoinColumn({ name: "account_id" })
   account: Account;
 
   @OneToMany(() => Order, (order) => order.livestream)
