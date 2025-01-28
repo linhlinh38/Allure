e29105f38215779010af59a4f9d13ff31156a05c
linhlinh38 <03linhnguyen@gmail.com>
2025-01-28 19:53:31 +0700

    implement result sheet

--- src/entities/statusTracking.entity.ts
+++ src/entities/statusTracking.entity.ts
@@ -13,33 +13,33 @@ import { Account } from "./account.entity";
 import { Brand } from "./brand.entity";
 import { Order } from "./order.entity";
 
-@Entity('status-trackings')
+@Entity("status_trackings")
 export class StatusTracking extends BaseEntity {
-  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Column({ type: "varchar", length: 255, nullable: true })
   reason: string;
 
-  @Column({ type: 'varchar', length: 255, nullable: false })
+  @Column({ type: "varchar", length: 255, nullable: false })
   status: string;
 
   @ManyToOne(() => Account)
-  @JoinColumn({ name: 'updated_by' })
+  @JoinColumn({ name: "updated_by" })
   updatedBy: Account;
 
   @ManyToOne(() => Account, (account) => account.statusTrackings, {
     nullable: true,
   })
-  @JoinColumn({ name: 'account' })
+  @JoinColumn({ name: "account" })
   account: Account;
 
   @ManyToOne(() => Brand, (brand) => brand.statusTrackings, {
     nullable: true,
   })
-  @JoinColumn({ name: 'brand' })
+  @JoinColumn({ name: "brand" })
   brand: Brand;
 
   @ManyToOne(() => Order, (order) => order.statusTrackings, {
     nullable: true,
   })
-  @JoinColumn({ name: 'order' })
+  @JoinColumn({ name: "order" })
   order: Order;
 }
