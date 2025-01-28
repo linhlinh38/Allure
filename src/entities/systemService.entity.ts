b6ccfbbe6dcc4d306849f7502107e5ca0fc633f1
linhlinh38 <03linhnguyen@gmail.com>
2025-01-28 19:51:03 +0700

    system service implement

---
+++ src/entities/systemService.entity.ts
@@ -0,0 +1,45 @@
+import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from "typeorm";
+import { Product } from "./product.entity";
+import { ServiceTypeEnum, StatusEnum } from "../utils/enum";
+import { BaseEntity } from "./base.entity";
+import { PreOrderProduct } from "./preOrderProduct.entity";
+import { CartItem } from "./cartItem.entity";
+import { Category } from "./category.entity";
+import { ConsultantService } from "./consultantService.entity";
+import { ResultSheet } from "./resultSheet.entity";
+
+@Entity("system_services")
+export class SystemService extends BaseEntity {
+  @Column({ type: "varchar" })
+  name: string;
+
+  @Column({ type: "varchar", nullable: true })
+  description: string;
+
+  @Column({ type: "varchar", nullable: true })
+  image: string;
+
+  @ManyToOne(() => Category, { nullable: true })
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
+  @Column({
+    type: "enum",
+    enum: ServiceTypeEnum,
+  })
+  type: ServiceTypeEnum;
+
+  @OneToMany(() => ConsultantService, (service) => service.systemService)
+  consultantServices?: ConsultantService[];
+
+  @ManyToOne(() => ResultSheet, (result) => result.systemServices)
+  @JoinColumn({ name: "result_sheet_id" })
+  resultSheet: ResultSheet;
+
+  @Column({
+    type: "enum",
+    enum: StatusEnum,
+    default: StatusEnum.ACTIVE,
+  })
+  status: StatusEnum;
+}
---
+++ src/routes/systemService.route.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import authentication from "../middleware/authentication";
+import validate from "../utils/validate";
+import systemServiceController from "../controllers/systemService.controller";
+import {
+  SystemServiceCreateSchema,
+  SystemServiceUpdateSchema,
+} from "../dtos/request/systemService.request";
+const systemServiceRouter = express.Router();
+
+systemServiceRouter.get("/", systemServiceController.getAll);
+
+systemServiceRouter.use(authentication);
+
+systemServiceRouter.post(
+  "/",
+  validate(SystemServiceCreateSchema),
+  systemServiceController.create
+);
+systemServiceRouter.put(
+  "/:id",
+  validate(SystemServiceUpdateSchema),
+  systemServiceController.update
+);
+export default systemServiceRouter;
