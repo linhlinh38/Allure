5834354c02e770aa3816c7c29d6e4ce9074bad93
linhlinh38 <03linhnguyen@gmail.com>
2025-02-09 13:48:49 +0700

    add check for group buying when add to cart

--- src/services/cartItem.service.ts
+++ src/services/cartItem.service.ts
@@ -149,13 +149,20 @@ class CartItemService extends BaseService<CartItem> {
   async checkCart(body: CartItem) {
     let isExisted = false;
     let data = body;
-    const check = await repository
+    const queryBuilder = await repository
       .createQueryBuilder("cartItem")
       .where("cartItem.account_id = :accountId", { accountId: body.account })
       .andWhere("cartItem.product_classification_id = :classification", {
         classification: body.productClassification,
-      })
-      .getMany();
+      });
+
+    if (body.groupBuying) {
+      queryBuilder.andWhere("cartItem.group_buying_id = :groupBuyingId", {
+        groupBuyingId: body.groupBuying,
+      });
+    }
+    const check = await queryBuilder.getMany();
+
     if (check.length !== 0) {
       data = check[0];
       data.quantity += body.quantity;
