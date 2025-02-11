ce7fab380b9700cf1080f088539f633f0a2abcfd
linhlinh38 <03linhnguyen@gmail.com>
2025-02-11 23:12:02 +0700

    adding service image

---
+++ src/entities/serviceImage.entity.ts
@@ -0,0 +1,44 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { BaseEntity } from "./base.entity";
+import { StatusEnum } from "../utils/enum";
+import { Product } from "./product.entity";
+import { ProductClassification } from "./productClassification.entity";
+import { Question } from "./question.entity";
+import { ConsultantService } from "./consultantService.entity";
+import { SystemService } from "./systemService.entity";
+
+@Entity("service_images")
+export class ServiceImage extends BaseEntity {
+  @Column({ type: "varchar", length: 100, nullable: true })
+  name?: string;
+
+  @Column({ name: "file_url", type: "varchar", length: 100, nullable: false })
+  fileUrl: string;
+
+  @Column({
+    type: "enum",
+    enum: StatusEnum,
+    default: StatusEnum.ACTIVE,
+  })
+  status: StatusEnum;
+
+  @ManyToOne(() => SystemService, (systemService) => systemService.images, {
+    nullable: true,
+  })
+  @JoinColumn({ name: "system_service_id" })
+  systemService: SystemService;
+
+  @ManyToOne(
+    () => ConsultantService,
+    (consultantService) => consultantService.images,
+    {
+      nullable: true,
+    }
+  )
+  @JoinColumn({ name: "consultant_service_id" })
+  consultantService: ConsultantService;
+
+  @ManyToOne(() => Question, (question) => question.images, { nullable: true })
+  @JoinColumn({ name: "question_id" })
+  question: Question;
+}
