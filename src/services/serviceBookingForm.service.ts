6a874615152b4b8875baec840299c5ead127a413
linhlinh38 <03linhnguyen@gmail.com>
2025-02-22 01:10:56 +0700

    update booking form status api

--- src/services/serviceBookingForm.service.ts
+++ src/services/serviceBookingForm.service.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../dataSource";
 import { Question } from "../entities/question.entity";
 import { ServiceBookingForm } from "../entities/serviceBookingForm.entity";
 import { ServiceImage } from "../entities/serviceImage.entity";
+import { StatusEnum } from "../utils/enum";
 import { BaseService } from "./base.service";
 
 const repository = AppDataSource.getRepository(ServiceBookingForm);
@@ -66,6 +67,58 @@ class ServiceBookingFormService extends BaseService<ServiceBookingForm> {
 
       await queryRunner.commitTransaction();
       return form;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async updateServiceBookingForm(
+    id: string,
+    status: StatusEnum
+  ): Promise<ServiceBookingForm> {
+    const queryRunner = AppDataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const serviceBookingFormRepository =
+        queryRunner.manager.getRepository(ServiceBookingForm);
+      const questionRepository = queryRunner.manager.getRepository(Question);
+      const serviceImageRepository =
+        queryRunner.manager.getRepository(ServiceImage);
+
+      const serviceBookingForm = await serviceBookingFormRepository.findOne({
+        where: { id },
+      });
+
+      if (!serviceBookingForm) {
+        throw new Error(`Form with id ${id} not found`);
+      }
+
+      await serviceBookingFormRepository.update(id, { status });
+
+      const questions = await questionRepository.find({
+        where: { serviceBookingForm: { id } },
+        relations: ["images"],
+      });
+
+      await questionRepository.update(
+        { serviceBookingForm: { id } },
+        { status }
+      );
+
+      for (const question of questions) {
+        await serviceImageRepository.update(
+          { question: { id: question.id } },
+          { status }
+        );
+      }
+
+      await queryRunner.commitTransaction();
+      return serviceBookingForm;
     } catch (error) {
       await queryRunner.rollbackTransaction();
       throw error;
