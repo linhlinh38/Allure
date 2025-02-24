a037b45ec877c70903f48558ccc90a9956e8c208
linhlinh38 <03linhnguyen@gmail.com>
2025-02-25 00:50:51 +0700

    update status api for system service

--- src/controllers/systemService.controller.ts
+++ src/controllers/systemService.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { systemServiceService } from "../services/systemService.service";
 import { createNormalResponse } from "../utils/response";
 import { NotFoundError } from "../errors/error";
+import { StatusEnum } from "../utils/enum";
 export default class SystemServiceController {
   static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
@@ -25,6 +26,20 @@ export default class SystemServiceController {
   static async update(req: Request, res: Response, next: NextFunction) {
     try {
       await systemServiceService.update(req.params.id, req.body);
+      return createNormalResponse(res, "Update service success");
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async updateStatus(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id, status } = req.params;
+      if (!Object.values(StatusEnum).includes(status as StatusEnum)) {
+        return res.status(400).json({ message: "Invalid status value" });
+      }
+      const statusEnum = StatusEnum[status as keyof typeof StatusEnum];
+      await systemServiceService.updateStatus(req.params.id, statusEnum);
       return createNormalResponse(res, "Update service success");
     } catch (err) {
       next(err);
