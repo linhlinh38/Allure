34e5f98bf7e2b91f6f61527e1b3a69a12e9864c0
Van Tho Tran <thotv.t1.1821@gmail.com>
2025-02-28 06:44:18 +0700

    fix system column

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -10,9 +10,9 @@ export abstract class BaseEntity extends TypeOrmBaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @CreateDateColumn({ type: 'timestamp with time zone' })
+  @CreateDateColumn({ type: 'timestamp with time zone', name: 'created_at' })
   createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp with time zone' })
+  @UpdateDateColumn({ type: 'timestamp with time zone', name: 'updated_at' })
   updatedAt: Date;
 }
