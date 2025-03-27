685adf28b2e64ecb00f65e054ce8ae52b829bf4e
Van Tho Tran <thotv.t1.1821@gmail.com>
2025-03-27 23:18:16 +0700

    fix test noti

--- src/controllers/FCM.controller.ts
+++ src/controllers/FCM.controller.ts
@@ -2,10 +2,6 @@ import { NextFunction, Response } from 'express';
 import { FCMService } from '../services/FCM.service';
 import { AuthRequest } from '../middleware/authentication';
 import { createNormalResponse } from '../utils/response';
-import { fcmTokenRepository } from '../repositories/fcmToken.repository';
-import { accountRepository } from '../repositories/account.repository';
-import { BadRequestError } from '../errors/error';
-import Logging from '../utils/Logging';
 
 export class FCMController {
   static async getToken(req: AuthRequest, res: Response, next: NextFunction) {
@@ -37,47 +33,13 @@ export class FCMController {
     next: NextFunction
   ) {
     try {
-      // Get account information
-      const account = await accountRepository.findOne({
-        where: { id: req.loginUser },
-      });
-
-      if (!account) {
-        throw new BadRequestError('Account not found');
-      }
-
-      // Get FCM token
-      const fcmToken = await fcmTokenRepository.findOne({
-        where: { account: { id: req.loginUser } },
-      });
-
-      if (!fcmToken) {
-        throw new BadRequestError('FCM token not found for this account');
-      }
-
-      // Send test notification
-      await FCMService.sendNotification(
-        fcmToken.token,
-        'Test Notification',
-        `Hello ${account.firstName}, this is a test notification!`,
-        {
-          type: 'TEST',
-          message: 'This is a test notification from the API',
-        }
+      const result = await FCMService.sendTestNotification(req.loginUser);
+      return createNormalResponse(
+        res,
+        'Test notification sent successfully',
+        result
       );
-
-      Logging.info(
-        `Test notification sent successfully to account ${account.id}`
-      );
-
-      return createNormalResponse(res, 'Test notification sent successfully', {
-        accountId: account.id,
-        accountName: account.firstName,
-        notificationSent: true,
-      });
     } catch (error) {
-      Logging.error('Error sending test notification:');
-      Logging.error(error);
       next(error);
     }
   }
