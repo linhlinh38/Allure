c853ab656be157dc0d9f6111b8eb562c88b44b60
linhlinh38 <03linhnguyen@gmail.com>
2025-03-31 22:03:15 +0700

    update report status

--- src/utils/enum.ts
+++ src/utils/enum.ts
@@ -248,7 +248,9 @@ export enum ReportTypeEnum {
 export enum ReportStatusEnum {
   PENDING = "PENDING",
   IN_PROCESSING = "IN_PROCESSING",
-  DONE = "DONE",
+  //DONE = "DONE",
+  REJECTED = "REJECTED",
+  APPROVED = "APPROVED",
   CANCELLED = "CANCELLED",
 }
 
