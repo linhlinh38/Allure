99a3a4d09f0305dc9bae2fbbb2b36eb9ca8d2ec0
Van Tho Tran <thotv.t1.1821@gmail.com>
2025-04-24 13:59:54 +0700

    fix eneity

--- src/entities/follow.entity.ts
+++ src/entities/follow.entity.ts
@@ -1,5 +1,6 @@
 import {
   Entity,
+  JoinColumn,
   ManyToOne,
 } from 'typeorm';
 import { Account } from './account.entity';
@@ -9,8 +10,10 @@ import { BaseEntity } from './base.entity';
 @Entity('follows')
 export class Follow extends BaseEntity {
   @ManyToOne(() => Account, (account) => account.follows)
+  @JoinColumn({ name: 'account_id' })
   account: Account;
 
   @ManyToOne(() => Brand, (brand) => brand.follows)
+  @JoinColumn({ name: 'brand_id' })
   brand: Brand;
 }
