e2ee992a71efaee06c3ba7f9b2b925d711c97f85
Van Tho Tran <thotv.t1.1821@gmail.com>
2025-04-26 01:05:28 +0700

    not authen filter filterPreOrderProducts

--- src/routes/preOrderProduct.route.ts
+++ src/routes/preOrderProduct.route.ts
@@ -22,11 +22,11 @@ preOrderProductRouter.get(
   PreOrderProductController.getPreOrderProductOfProduct
 );
 preOrderProductRouter.get('/', PreOrderProductController.getAll);
-preOrderProductRouter.use(authentication);
 preOrderProductRouter.get(
   '/filter-pre-order-product',
   PreOrderProductController.filterPreOrderProducts
 );
+preOrderProductRouter.use(authentication);
 
 preOrderProductRouter.post(
   "/",
