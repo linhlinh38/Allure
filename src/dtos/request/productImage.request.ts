2b80b5a0c73cbfc99ebec472604b9f1de55bea98
Van Tho Tran <thotv.t1.1821@gmail.com>
2025-05-04 16:07:10 +0700

    name nullable

--- src/dtos/request/productImage.request.ts
+++ src/dtos/request/productImage.request.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 
 export const ProductImageCreateSchema = z.object({
   body: z.object({
-    name: z.string().max(255, 'Name cannot exceed 255 characters').optional(),
+    name: z
+      .string()
+      .max(255, 'Name cannot exceed 255 characters')
+      .nullable()
+      .optional(),
     fileUrl: z.string().max(255),
     product: z.string().optional(),
   }),
